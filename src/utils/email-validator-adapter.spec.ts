c4f48c4ccbc724595a1e762ae3a68d862d8c4eb7
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2023-12-29 16:58:32 -0300

    test: ensure EmailValidatorAdapter returns false if validator returns true

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,9 +1,22 @@
 import { EmailValidatorAdapter } from './email-validator'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isEmail (): boolean { 
+    return true
+  }
+}))
 
 describe('EmailValidatorAdapter', () => {
   it('Should return false if validator returns false ', () => {
     const sut = new EmailValidatorAdapter()
-    const isValid = sut.isValid('invalid_email@email.com')
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+    const isValid = sut.isValid('invalid_email@gmail.com')
     expect(isValid).toBe(false)
+  })
+  it('Should return false if validator returns false ', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('valid_email@email.com')
+    expect(isValid).toBe(true)
   })
 })
