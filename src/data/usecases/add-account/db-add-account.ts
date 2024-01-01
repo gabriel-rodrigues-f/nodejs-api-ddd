7e291ad42e69bc3c3791b15bc8fbd26b5aca0ee8
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-01 20:25:02 -0300

    feat: ensure DbAddAccount calls AddAccountRepository with correct values

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -8,12 +8,7 @@ export class DbAddAccount implements AddAccount {
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const hashedPassword = await this.encrypter.encrypt(accountData.password)
-    await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
-    return await new Promise(resolve => resolve({
-      name: 'valid_name',
-      email: 'valid_email',
-      password: 'valid_password',
-      id: 'valid_id'
-    }))
+    const account = await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
+    return account
   }
 }
