b542ee86b5b45d6195eee2cc9fb17fcd7615ab03
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-02 16:33:49 -0300

    feat: ensure cors is enable

--- src/main/middlewares/body-parser.test.ts
+++ src/main/middlewares/body-parser.test.ts
@@ -1,7 +1,7 @@
 import request from 'supertest'
 import app from '../config/app'
 
-describe('Bosy Parser Middleware', () => {
+describe('Body Parser Middleware', () => {
   it('Should parse body as JSON ', async () => {
     app.post('/test_body_parser', (req, res) => {
       res.send(req.body)
