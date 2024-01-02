d1186a2b3368448dbd37732ea75512440b68463e
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-02 16:27:50 -0300

    feat: ensure requests parse body as JSON

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,14 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Bosy Parser Middleware', () => {
+  it('Should parse body as JSON ', async () => {
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body)
+    })
+    await request(app)
+      .post('/test_body_parser')
+      .send({ name: 'valid_name' })
+      .expect({ name: 'valid_name' })
+  })
+})
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,4 @@
-import express from 'express'
+import app from './config/app'
 
-const app = express()
 const PORT = process.env.PORT || 5050
 app.listen(PORT, () => console.log(`Server running at ${PORT}`))
