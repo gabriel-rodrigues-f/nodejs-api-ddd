04835a3cc5dc78203c26cd378eee6c0fd0fe2972
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-03 11:10:31 -0300

    test: ensure LogControllerDecorator returns same result of the Controller

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -40,4 +40,19 @@ describe('Log Controller Decorator', () => {
     await sut.handle(httpRequest)
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
   })
+
+  it('Should return same result of the controller ', async () => {
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const { sut, controllerStub } = makeSut()
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual({ body: {}, statusCode: 200 })
+  })
 })
