1d42aa4732fc5ac0d38ef78980adbaf4e77af80f
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-03 19:57:41 -0300

    feat: ensure LoginController return 400 if no login email is provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,5 +1,17 @@
-describe('Login Controller', () => {
-  test('Should return 400 if no email is provided', () => {
+import { MissingParamError } from "../../errors"
+import { badRequest } from "../../helpers/http-helpers"
+import { HttpRequest } from "../../protocols"
+import { LoginController } from "./login"
 
+describe('Login Controller', () => {
+  test('Should return 400 if no email is provided', async () => {
+    const request: HttpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const sut = new LoginController()
+    const httpResponse = await sut.handle(request)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
   })
 })
