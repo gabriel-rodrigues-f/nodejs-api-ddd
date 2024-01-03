5e80edcd5d9f9c8a01a39d4721a9518acfc8ec88
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-03 20:01:51 -0300

    test: ensure LoginController return 400 if no login password is provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -3,6 +3,10 @@ import { badRequest } from "../../helpers/http-helpers"
 import { HttpRequest } from "../../protocols"
 import { LoginController } from "./login"
 
+const makeSut = (): LoginController => {
+  return new LoginController()
+}
+
 describe('Login Controller', () => {
   test('Should return 400 if no email is provided', async () => {
     const request: HttpRequest = {
@@ -10,8 +14,19 @@ describe('Login Controller', () => {
         password: 'any_password'
       }
     }
-    const sut = new LoginController()
+    const sut = makeSut()
     const httpResponse = await sut.handle(request)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
+  })
+
+  test('Should return 400 if no password is provided', async () => {
+    const request: HttpRequest = {
+      body: {
+        email: 'any_email@mail.com'
+      }
+    }
+    const sut = makeSut()
+    const httpResponse = await sut.handle(request)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
   })
 })
