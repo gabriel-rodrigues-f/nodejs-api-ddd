47780f8fc66634db396a00c514a762201717cc5b
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-03 20:50:45 -0300

    test: ensure LoginController returns 500 if invalid email is provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from "../../errors"
-import { badRequest } from "../../helpers/http-helpers"
+import { badRequest, serverError } from "../../helpers/http-helpers"
 import { HttpRequest } from "../../protocols"
 import { EmailValidator } from "../signup/signup-protocols"
 import { LoginController } from "./login"
@@ -17,7 +17,7 @@ const makeEmailValidator = (): EmailValidator => {
 const makeFakeRequest = (): HttpRequest => ({
   body: {
     email: 'any_email@mail.com',
-      password: 'any_password'
+    password: 'any_password'
   }
 })
 
@@ -70,5 +70,14 @@ describe('Login Controller', () => {
     jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(badRequest(new InvalidParamError('email')))
+  })
+
+  test('Should return 500 if EmailValidator throws', async () => {
+    const { sut, emailValidatorStub } = makeSut()
+    jest.spyOn(emailValidatorStub, 'isValid').mockImplementationOnce(() => {
+      throw new Error()
+    })
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
