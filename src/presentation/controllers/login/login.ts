efd92291188fc612dbae90bb99c34e4181015664
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-03 21:44:03 -0300

    feat: ensure LoginController returns 200 if valid credentials are provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest, serverError, unauthorized } from '../../helpers/http-helpers'
+import { ok, badRequest, serverError, unauthorized } from '../../helpers/http-helpers'
 import { Authentication, EmailValidator, Controller, HttpRequest, HttpResponse } from './login-protocols'
 
 export class LoginController implements Controller {
@@ -25,6 +25,7 @@ export class LoginController implements Controller {
       if (!accessToken) {
         return unauthorized()
       }
+      return ok({ accessToken })
     } catch (error) {
       return serverError(error)
     }
