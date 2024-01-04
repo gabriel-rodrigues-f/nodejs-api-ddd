8876a8604ddf35194a40d0a13e5b171a252a3cc5
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-04 14:48:19 -0300

    test: ensure SignUpController returns 400 if validation return error

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -191,4 +191,11 @@ describe('SignUp Controller', () => {
     sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
   })
+
+  test('Should return an Error if Validation returns an Error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
+  })
 })
