ed0353e6eba26eb518bb5b4b04e4bf6971a1d75b
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-05 18:20:58 -0300

    test: add login routes

--- src/main/routes/login-routes.spec.ts
+++ src/main/routes/login-routes.spec.ts
@@ -1,6 +1,10 @@
 import request from 'supertest'
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
+
+let accountCollection: Collection
 
 describe('Login Route', () => {
   beforeAll(async () => {
@@ -12,7 +16,7 @@ describe('Login Route', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    accountCollection = MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -25,6 +29,24 @@ describe('Login Route', () => {
           email: 'valid_email@mail.com',
           password: '123',
           passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return 200 on login', async () => {
+      const password = await hash('123', 12)
+      await accountCollection.insertOne({
+        name: 'valid_name',
+        email: 'valid_email@mail.com',
+        password,
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'valid_email@mail.com',
+          password: '123',
         })
         .expect(200)
     })
