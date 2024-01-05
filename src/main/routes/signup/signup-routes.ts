a9807bc882313814bc70c32cf72def16b84e3047
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-05 15:05:19 -0300

    refactor: rename signup routes with login routes

--- src/main/routes/signup/signup-routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express'
-import { makeSignUpController } from '../../factories/signup/signup-factory'
-import { adaptRoute } from '../../adapters/express/express-route-adapter'
-
-export default (router: Router): void => {
-  router.post('/signup', adaptRoute(makeSignUpController()))
-}
