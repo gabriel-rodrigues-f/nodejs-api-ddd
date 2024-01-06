62a55da13942d8e6773ad54f0899187885f42756
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-06 11:33:09 -0300

    fix: docker-compose mongo connection

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,5 @@
 export default {
-  // MONGO_DB_URL: process.env.MONGO_DB_URL || 'mongodb://mmongo:27017/nodejs-api-ddd',
-  MONGO_DB_URL: process.env.MONGO_DB_URL || 'mongodb+srv://gabrielvrfreire:VEzXpsUKeImteFu4@cluster0.rhhrdat.mongodb.net/?retryWrites=true&w=majority',
+  MONGO_DB_URL: process.env.MONGO_DB_URL || 'mongodb://mongo:27017/nodejs-api-ddd',
   MONGO_DB_DATABASE: process.env.MONGO_DB_DADABASE || 'nodejs-api-ddd',
   PORT: process.env.PORT || 5050,
   JWT_SECRET: process.env.JWT_SECRET || ';alksjdfjklajdf'
