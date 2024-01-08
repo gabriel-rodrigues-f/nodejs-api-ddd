e750ded63c341fecafeb75d5eb7cb73fe9d78905
Gabriel Silva <loktwo@outlook.com>
2024-01-07 21:56:16 -0300

    chore: dynamic ip address for replace inventory variable

--- infra-tf/5 - output.tf
+++ infra-tf/5 - output.tf
@@ -3,7 +3,14 @@ output "instance_ip_addr" {
 }
 
 # Inventory for Ansible
-resource "local_file" "inventory" {
-    content = aws_instance.fiap.public_ip
+data "template_file" "ansible_inventory" {
+    template = file("ansible/inventory.tmpl")
+    vars = {
+        instance_ip = aws_instance.fiap.public_ip
+    }
+}
+
+resource "local_file" "ansible_inventory" {
+    content = data.template_file.ansible_inventory.rendered
     filename = "ansible/inventory.ini"
 }
