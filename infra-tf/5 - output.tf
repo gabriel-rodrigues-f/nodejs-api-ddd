52028f54cecb03f81cf8ff75cc1093d02b011db7
Gabriel Silva <loktwo@outlook.com>
2024-01-07 21:18:13 -0300

    chore: adding ip address to ansible inventory

--- infra-tf/5 - output.tf
+++ infra-tf/5 - output.tf
@@ -1,3 +1,9 @@
 output "instance_ip_addr" {
     value = aws_instance.fiap.public_ip
 }
+
+# Inventory for Ansible
+resource "local_file" "inventory" {
+    content = aws_instance.fiap.public_ip
+    filename = "ansible/inventory.ini"
+}
