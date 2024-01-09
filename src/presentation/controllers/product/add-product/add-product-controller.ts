75966b9921cf1a9798e6ff7096cccefb01a1d14d
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-09 14:23:39 -0300

    feat: ensure AddProductController returns a badRequest error if validation fails

--- src/presentation/controllers/product/add-product/add-product-controller.ts
+++ src/presentation/controllers/product/add-product/add-product-controller.ts
@@ -1,9 +1,13 @@
+import { badRequest } from '../../../helpers/http/http-helpers'
 import { Controller, HttpRequest, HttpResponse, Validation } from './add-product-controller-protocols'
 
 export class AddProductController implements Controller {
   constructor (private readonly validation: Validation) { }
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    this.validation.validate(httpRequest.body)
+    const error = this.validation.validate(httpRequest.body)
+    if (error) {
+      return badRequest(error)
+    }
     return new Promise(resolve => resolve(null))
   }
 }
