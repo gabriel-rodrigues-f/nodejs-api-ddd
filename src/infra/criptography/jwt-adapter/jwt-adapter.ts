e779744a3e2869a07341dec53ed22040c0ebed0b
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-13 12:06:55 -0300

    feat: ensure JwtAdapter return decrypted value on verify success

--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -9,8 +9,8 @@ export class JwtAdapter implements Encrypter, Decrypter {
     return accessToken
   }
 
-  async decrypt (value: string): Promise<string> {
-    jwt.verify(value, this.secret)
-    return null
+  async decrypt (token: string): Promise<string> {
+    const value: any = jwt.verify(token, this.secret)
+    return value
   }
 }
