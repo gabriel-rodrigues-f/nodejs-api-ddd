ff9de6371b0154ea095320ada0ece93d038f1414
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-13 12:28:36 -0300

    test: ensure AccountMongoRepositoriy find an account by token without role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -61,7 +61,7 @@ describe('Account Mongo Repository', () => {
     })
   })
 
-  describe('loadByEmail()', () => {
+  describe('updateAccessToken()', () => {
     test('Should udpate the account accessToken on updateAccessToken success', async () => {
       const sut = makeSut()
       const result = await accountCollection.insertOne(makeAddAccountModel())
@@ -76,6 +76,24 @@ describe('Account Mongo Repository', () => {
         expect(accountWithAccessToken).toBeTruthy()
         expect(accountWithAccessToken.accessToken).toBe('any_token')
       }
+    })
+  })
+
+  describe('loadByToken()', () => {
+    test('Should return an account on loadByToken without role', async () => {
+      const sut = makeSut()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        accessToken: 'any_token'
+      })
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
     })
   })
 })
