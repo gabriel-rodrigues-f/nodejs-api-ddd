b1e287ba4ee578f00979c1fe306bc3f9536a3fa8
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-13 12:13:36 -0300

    test: ensure JwtAdapter throws if verify throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -23,6 +23,15 @@ describe('JWT Adapter', () => {
       expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
     })
 
+    test('Should throw if sign throws', async () => {
+      const sut = makeSut()
+      jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+        throw new Error()
+      })
+      const promise = sut.encrypt('any_token')
+      await expect(promise).rejects.toThrow()
+    })
+
     test('Should return a token on sign success', async () => {
       const sut = makeSut()
       const accessToken = await sut.encrypt('any_id')
@@ -51,6 +60,15 @@ describe('JWT Adapter', () => {
       const sut = makeSut()
       const value = await sut.decrypt('any_token')
       expect(value).toBe('any_value')
+    })
+
+    test('Should throw if verify throws', async () => {
+      const sut = makeSut()
+      jest.spyOn(jwt, 'verify').mockImplementationOnce(() => {
+        throw new Error()
+      })
+      const promise = sut.decrypt('any_token')
+      await expect(promise).rejects.toThrow()
     })
   })
 })
