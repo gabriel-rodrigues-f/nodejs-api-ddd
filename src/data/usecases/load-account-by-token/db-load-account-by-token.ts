05625b8265cc12c1d7f50332f6062411a7ba521e
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-13 11:33:36 -0300

    feat: ensure DbLoadAccountByToken calls LoadAccountByTokenRepository usign correct values

--- src/data/usecases/load-account-by-token/db-load-account-by-token.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.ts
@@ -1,11 +1,18 @@
 import { LoadAccountByToken } from '../../../domain/usecases/load-account-by-token'
 import { Decrypter } from '../../protocols/criptography/decrypter'
-import { AccountModel } from '../add-account/db-add-account-protocols'
+import { AccountModel, LoadAccountByTokenRepository } from '../add-account/db-add-account-protocols'
 
 export class DbLoadAccountByToken implements LoadAccountByToken {
-  constructor (private readonly decrypt: Decrypter) { }
+  constructor (
+    private readonly decrypt: Decrypter,
+    private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository
+  ) { }
+
   async load (accessToken: string, role?: string): Promise<AccountModel> {
-    await this.decrypt.decrypt(accessToken)
+    const token = await this.decrypt.decrypt(accessToken)
+    if (token) {
+      await this.loadAccountByTokenRepository.loadByToken(accessToken, role)
+    }
     return new Promise(resolve => resolve(null))
   }
 }
