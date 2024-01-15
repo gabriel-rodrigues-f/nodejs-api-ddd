1aa96587e723e1554d9e785b6012dbd27b94cde1
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-15 00:37:08 -0300

    feat: ensure DbLoadProducts calls LoadProductsRepository

---
+++ src/data/protocols/db/product/load-products-repository.ts
@@ -0,0 +1,5 @@
+import { ProductModel } from '../../../../domain/models/product'
+
+export interface LoadProductsRepository {
+  loadAll (): Promise<ProductModel[]>
+}
---
+++ src/data/usecases/load-products/db-load-products.ts
@@ -0,0 +1,10 @@
+import { LoadProductsRepository } from '../../protocols/db/product/load-products-repository'
+import { ProductModel } from '../add-product/db-add-product-protocols'
+
+export class DbLoadProducts implements LoadProductsRepository {
+  constructor (private readonly loadProductsRepository: LoadProductsRepository) { }
+  async loadAll (): Promise<ProductModel[]> {
+    await this.loadProductsRepository.loadAll()
+    return []
+  }
+}
