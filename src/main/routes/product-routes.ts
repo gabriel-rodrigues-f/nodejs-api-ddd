d3345beb56845aa9ddf80e60bc024d43bc4f02f2
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-15 02:03:19 -0300

    feat: ensure load products route returns 204 on success without products in database

--- src/main/routes/product-routes.ts
+++ src/main/routes/product-routes.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeAddProductController } from '../factories/controllers/product/add-product/add-product-controller-factory'
-import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
-import { adaptMiddleware } from '../adapters/express-middleware-adapter'
 import { makeLoadProductsController } from '../factories/controllers/product/add-product/load-products/load-products-controller-factory'
+import { adminAuth } from '../middlewares/admin-auth'
+import { auth } from '../middlewares/auth'
 
 export default (router: Router): void => {
-  const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
-  const auth = adaptMiddleware(makeAuthMiddleware('user'))
   router.post('/products', adminAuth, adaptRoute(makeAddProductController()))
   router.get('/products', auth, adaptRoute(makeLoadProductsController()))
 }
