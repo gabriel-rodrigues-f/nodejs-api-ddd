6f7cba0147c8a43a28f0d464e6ce8159396b602e
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-15 00:01:04 -0300

    feat: ensure LoadProductsController return 200 on success

--- src/presentation/controllers/product/load-products/load-products-controller-protocols.ts
+++ src/presentation/controllers/product/load-products/load-products-controller-protocols.ts
@@ -1,3 +1,4 @@
 export * from '../../../protocols'
 export * from '../../../../domain/usecases/load-products'
 export * from '../../../../domain/models/product'
+export * from '../../../../presentation/helpers/http/http-helpers'
--- src/presentation/controllers/product/load-products/load-products-controller.ts
+++ src/presentation/controllers/product/load-products/load-products-controller.ts
@@ -1,9 +1,9 @@
-import { Controller, HttpRequest, HttpResponse, LoadProducts } from '../load-products/load-products-controller-protocols'
+import { Controller, HttpRequest, HttpResponse, LoadProducts, ok } from '../load-products/load-products-controller-protocols'
 
 export class LoadProductsController implements Controller {
   constructor (private readonly loadProducts: LoadProducts) { }
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadProducts.load()
-    return null
+    const products = await this.loadProducts.load()
+    return ok(products)
   }
 }
