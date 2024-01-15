85a2fc466ab99d8594f678b34524995189c8da27
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-15 00:20:43 -0300

    feat: ensure LoadProductsController return 204 if LoadProduct returns empty

--- src/presentation/controllers/product/load-products/load-products-controller.ts
+++ src/presentation/controllers/product/load-products/load-products-controller.ts
@@ -1,11 +1,11 @@
-import { Controller, HttpRequest, HttpResponse, LoadProducts, ok, serverError } from '../load-products/load-products-controller-protocols'
+import { Controller, HttpRequest, HttpResponse, LoadProducts, noContent, ok, serverError } from '../load-products/load-products-controller-protocols'
 
 export class LoadProductsController implements Controller {
   constructor (private readonly loadProducts: LoadProducts) { }
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const products = await this.loadProducts.load()
-      return ok(products)
+      return products.length ? ok(products) : noContent()
     } catch (error) {
       return serverError(error)
     }
