a73d52dc2850050a7f02e8bc15e3a37ac84c962e
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-16 00:52:47 -0300

    feat: ensure LoadProductByIdController returns 403 if invalid productId is provided

--- src/presentation/controllers/product/load-product-by-id/load-product-by-id-controller-protocols.ts
+++ src/presentation/controllers/product/load-product-by-id/load-product-by-id-controller-protocols.ts
@@ -1,3 +1,5 @@
 export * from '@/domain/models/product'
+export * from '@/domain/usecases/load-product-by-id'
 export * from '@/presentation/helpers/http/http-helpers'
 export * from '../../../protocols'
+export * from '@/presentation/errors'
--- src/presentation/controllers/product/load-product-by-id/load-product-by-id-controller.ts
+++ src/presentation/controllers/product/load-product-by-id/load-product-by-id-controller.ts
@@ -1,11 +1,13 @@
-import { type LoadProductById } from '@/domain/usecases/load-product-by-id'
-import { type HttpResponse, type Controller } from './load-product-by-id-controller-protocols'
+import { InvalidParamError, type LoadProductById, type HttpResponse, type Controller, forbidden } from './load-product-by-id-controller-protocols'
 
 export class LoadProductByidController implements Controller {
   constructor (private readonly loadProductById: LoadProductById) { }
   async handle (request: any): Promise<HttpResponse> {
     const { productId } = request.params
-    await this.loadProductById.loadById(productId)
-    return await Promise.resolve(null)
+    const product = await this.loadProductById.loadById(productId)
+    if (!product) {
+      return forbidden(new InvalidParamError('productId'))
+    }
+    return null
   }
 }
