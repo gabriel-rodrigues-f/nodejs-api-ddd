1b8e9b50bf43f2b29a33348301828f6e8285643d
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-16 13:39:07 -0300

    feat: ensure LoadAccountByCpfController returns 500 if LoadAccountByCpf throws

--- src/presentation/controllers/account/load-account-by-cpf-controller.ts
+++ src/presentation/controllers/account/load-account-by-cpf-controller.ts
@@ -1,13 +1,16 @@
-import { notFound, type Controller, type HttpResponse, type LoadAccountByCpf } from './load-account-by-cpf-controller-protocols'
+import { notFound, type Controller, type HttpResponse, type LoadAccountByCpf, serverError } from './load-account-by-cpf-controller-protocols'
 
 export class LoadAccountByCpfController implements Controller {
   constructor (private readonly loadAccountByCpf: LoadAccountByCpf) { }
   async handle (request: any): Promise<HttpResponse> {
-    const { id: cpf } = request.params
-    const exists = await this.loadAccountByCpf.loadByCpf(cpf)
-    if (!exists) {
-      return notFound()
+    try {
+      const { id: cpf } = request.params
+      const exists = await this.loadAccountByCpf.loadByCpf(cpf)
+      if (!exists) {
+        return notFound()
+      }
+    } catch (error) {
+      return serverError(error)
     }
-    return await Promise.resolve(null)
   }
 }
