9c984c8d34fff1a8176de78a3296745c90d83634
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-16 13:36:39 -0300

    feat: ensure LoadAccountByCpfController returns 404 if no account is found

--- src/presentation/controllers/account/load-account-by-cpf-controller.ts
+++ src/presentation/controllers/account/load-account-by-cpf-controller.ts
@@ -1,10 +1,13 @@
-import { type Controller, type HttpResponse, type LoadAccountByCpf } from './load-account-by-cpf-controller-protocols'
+import { notFound, type Controller, type HttpResponse, type LoadAccountByCpf } from './load-account-by-cpf-controller-protocols'
 
 export class LoadAccountByCpfController implements Controller {
   constructor (private readonly loadAccountByCpf: LoadAccountByCpf) { }
   async handle (request: any): Promise<HttpResponse> {
     const { id: cpf } = request.params
-    await this.loadAccountByCpf.loadByCpf(cpf)
+    const exists = await this.loadAccountByCpf.loadByCpf(cpf)
+    if (!exists) {
+      return notFound()
+    }
     return await Promise.resolve(null)
   }
 }
--- src/presentation/helpers/http/http-helpers.ts
+++ src/presentation/helpers/http/http-helpers.ts
@@ -26,6 +26,11 @@ export const forbidden = (error: Error): HttpResponse => ({
   body: error
 })
 
+export const notFound = (): HttpResponse => ({
+  statusCode: 404,
+  body: null
+})
+
 export const serverError = (error: Error): HttpResponse => ({
   statusCode: 500,
   body: new ServerError(error.stack)
