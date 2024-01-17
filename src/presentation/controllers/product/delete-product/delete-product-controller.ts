0e4a2b82df5f318bbb3e62e28225d6de40130cca
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-17 15:33:19 -0300

    test:  ensure DeleteProductController returns 500 if DeleteProduct throws

--- src/presentation/controllers/product/delete-product/delete-product-controller.ts
+++ src/presentation/controllers/product/delete-product/delete-product-controller.ts
@@ -1,14 +1,19 @@
 import {
   type DeleteProduct,
   type Controller,
-  type HttpResponse
+  type HttpResponse,
+  serverError
 } from '.'
 
 export class DeleteProductController implements Controller {
   constructor (private readonly deleteProduct: DeleteProduct) { }
   async handle (request: any): Promise<HttpResponse> {
-    const { id } = request.params
-    await this.deleteProduct.delete(id)
-    return await Promise.resolve(null)
+    try {
+      const { id } = request.params
+      await this.deleteProduct.delete(id)
+      return await Promise.resolve(null)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
