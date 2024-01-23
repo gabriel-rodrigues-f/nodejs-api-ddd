5745dc2d7c8dc2cc8e67f7234f000c99ea0d5e7b
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-23 15:27:52 -0300

    feat: ensure AddOrderController returns 400 on validation fails

--- src/presentation/controllers/add-order-controller.ts
+++ src/presentation/controllers/add-order-controller.ts
@@ -3,11 +3,13 @@ import {
   type Controller,
   type Validation
 } from '@/presentation/protocols'
+import { badRequest } from '../helpers'
 
 export class AddOrderController implements Controller {
   constructor (private readonly validation: Validation) { }
   async handle (request: any): Promise<HttpResponse> {
-    this.validation.validate(request.body)
+    const error = this.validation.validate(request.body)
+    if (error) return badRequest(error)
     return await Promise.resolve(null)
   }
 }
