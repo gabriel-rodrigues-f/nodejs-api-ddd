dae74c4a3b45c78177a86714746722cc8d4c1625
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-24 09:52:26 -0300

    feat: ensure UpdateOrderController calls Validation with correct values

---
+++ src/presentation/controllers/update-order-controller.ts
@@ -0,0 +1,13 @@
+import {
+  type HttpResponse,
+  type Controller,
+  type Validation
+} from '@/presentation/protocols'
+
+export class UpdateOrderController implements Controller {
+  constructor (private readonly validation: Validation) { }
+  async handle (request: any): Promise<HttpResponse> {
+    this.validation.validate(request.body)
+    return await Promise.resolve(null)
+  }
+}
