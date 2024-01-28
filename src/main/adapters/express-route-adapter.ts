fa4a45ddb07fbd1b31e0d4942d4d94b59e1f532b
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-27 22:09:59 -0300

    feat: log request and response in console

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -1,5 +1,6 @@
 import { type Controller, type HttpRequest } from '@/presentation/protocols'
 import { type Request, type Response } from 'express'
+import clc from 'cli-color'
 
 export const adaptRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
@@ -17,5 +18,25 @@ export const adaptRoute = (controller: Controller) => {
         error: response.body.message
       })
     }
+
+    const statusCodeColor = (statusCode: string): any => {
+      if (statusCode.startsWith('2')) return clc.green(statusCode)
+      if (statusCode.startsWith('4') || statusCode.startsWith('5')) return clc.red(statusCode)
+      return clc.yellow(statusCode)
+    }
+
+    process.stdout.write(`
+      \r${clc.blue('Date:')} ${clc.blackBright(new Date().toLocaleString())}
+      \r${clc.blue('Status code:')} ${statusCodeColor(response.statusCode.toString())}
+      \r${clc.blue('Method:')} ${clc.blackBright(req.method)}
+      \r${clc.blue('Host')} ${clc.blackBright(req.hostname)}
+      \r${clc.blue('Base URL')} ${clc.blackBright(req.baseUrl)}
+      \r${clc.blue('HTTP Resource:')} ${clc.blackBright(req.path)}
+      \r${clc.blue('HTTP Params:')} ${clc.blackBright(JSON.stringify(req.params))}
+      \r${clc.blue('Headers:')} ${clc.blackBright(JSON.stringify(req.headers))}
+      \r${clc.blue('Request Body:')} ${clc.white(JSON.stringify(req.body))}
+      \r${clc.blue('Response Body:')} ${clc.white(JSON.stringify(response.body)
+    )}
+    `)
   }
 }
