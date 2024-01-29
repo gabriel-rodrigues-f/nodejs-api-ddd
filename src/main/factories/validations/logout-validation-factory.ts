97dce4ed0a29389db042a0788afc1d015897deab
gabriel-rodrigues-f <gabrielvinicius.rodrigues@outlook.com>
2024-01-29 00:50:12 -0300

    feat: ensure validation using accessToken

---
+++ src/main/factories/validations/logout-validation-factory.ts
@@ -0,0 +1,10 @@
+import { RequiredFieldsValidation, ValidationComposite } from '@/validation/validators'
+import { type Validation } from '@/presentation/protocols'
+
+export const makeLogoutValidation = (): ValidationComposite => {
+  const validations: Validation[] = []
+  for (const field of ['accessToken']) {
+    validations.push(new RequiredFieldsValidation(field))
+  }
+  return new ValidationComposite(validations)
+}
---
+++ tests/main/factories/logout-validation-factory.spec.ts
@@ -0,0 +1,19 @@
+import { type Validation } from '@/presentation/protocols'
+import { makeLogoutValidation } from '@/main/factories/validations'
+import {
+  RequiredFieldsValidation,
+  ValidationComposite
+} from '@/validation/validators'
+
+jest.mock('@/validation/validators/validation-composite')
+
+describe('Logout Validation Factory', () => {
+  test('Should call validation with all validations ', () => {
+    makeLogoutValidation()
+    const validations: Validation[] = []
+    for (const field of ['accessToken']) {
+      validations.push(new RequiredFieldsValidation(field))
+    }
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
